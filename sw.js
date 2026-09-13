fc7a7be882a7216e6347645e354af6a77622efe8
vhuang904 <vhuang904@gmail.com>
2026-09-13 11:55:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  */
 
 // 👉 每次重大發布修改此版本號，強制手機客戶端熱更新
-const CACHE_VERSION = '20260912-v41.3';
+const CACHE_VERSION = '20260912-v41.4';
 const STATIC_CACHE_NAME = `bigv-static-${CACHE_VERSION}`;
 const IMAGE_CACHE_NAME = `bigv-images-${CACHE_VERSION}`;
 
