e83db016cb4fad556d1b095831bf78b28a7dd1ce
vhuang904 <vhuang904@gmail.com>
2026-09-14 11:06:59 +0800

    Update service worker version and caching strategies

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  */
 
 // 👉 每次重大發布修改此版本號，強制手機客戶端熱更新
-const CACHE_VERSION = '20260913-v43.7.9';
+const CACHE_VERSION = '20260913-v43.8.0';
 const STATIC_CACHE_NAME = `bigv-static-${CACHE_VERSION}`;
 const IMAGE_CACHE_NAME = `bigv-images-${CACHE_VERSION}`;
 
